b5616fde2b526265c726da1657d24da207688113
Bernat <bernatmv@gmail.com>
2016-11-23 18:56:15 +0100

    FW: improving building process

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,34 @@
+console.log(1);
+import io from 'socket.io-client';
+console.log(2);
+console.log(io);
 
-export default function hello(name) {
-  return (() => `hello, ${name}`)();
+function play() {
+    console.log('play');
+
+    const janken_endpoint = 'http://localhost';
+
+    const socket = io.connect(janken_endpoint);
+    socket.on('connect', () => {
+        console.log("connected");
+        socket.close();
+    });
+    socket.on('connect_failed', () => {
+        console.log("connect_failed");
+        socket.close();
+    });
+
+    socket.on('message', (data) => console.log(data));
+
+    socket.on('disconnect', () => {
+        console.log("disconnected");
+        socket.close();
+    });
 }
+
+window.onload = () => {
+    console.log('window.onload');
+    play();
+};
+
+console.log('EOF');
