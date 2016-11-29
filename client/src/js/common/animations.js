ec3d7f9bd01ee7a2f2265287629af91b3801fc5b
Bernat <bernatmv@gmail.com>
2016-11-29 17:29:13 +0100

    fixed problem with blinking tween

--- client/src/js/common/animations.js
+++ client/src/js/common/animations.js
@@ -1,16 +1,23 @@
-import random from './random';
+import { Phaser } from 'phaser';
 
-export const blinkTween = (game, element, speed = 800, loop = true, start = true) => {
+export function blinkTween(game, element, speed = 800, loop = true, start = true) {
     const tween = game.add.tween(element)
-        .to({
-            alpha: 0
-        }, speed, Phaser.Easing.Linear.None)
-        .to({
-            alpha: 1
-        }, speed, Phaser.Easing.Linear.None);
-    if (loop)
+        .to({alpha: 0}, speed, Phaser.Easing.Linear.None)
+        .to({alpha: 1}, speed, Phaser.Easing.Linear.None);
+    if (loop) 
         tween.loop();
     if (start)
         tween.start();
     return tween;
-};
+}
+
+export function beatTween(game, element, speed = 400, loop = true, start = true) {
+    const tween = game.add.tween(element.scale)
+        .to({x: 1.2, y: 1.2}, speed, Phaser.Easing.Linear.None)
+        .to({x: 1, y: 1}, speed, Phaser.Easing.Linear.None);
+    if (loop) 
+        tween.loop();
+    if (start)
+        tween.start();
+    return tween;
+}
--- client/src/js/states/main.js
+++ client/src/js/states/main.js
@@ -1,6 +1,9 @@
 import { State } from 'phaser';
 import { startGame, play, joinGame } from '../actions';
 import { blinkTween } from '../common/animations';
+import {
+    gradients, addGradient
+} from '../common/background';
 
 class Main extends State {
 
@@ -23,15 +26,13 @@ class Main extends State {
         const gameId = 'gameStarted';
         const { eventStream, connectionStream, sendCommand } =  this.game.api;
 
+        // background
+        addGradient(this.game);
         // text
         const title = this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Janken!', style);
         title.anchor.x = .5;
-        title.anchor.y = .5;
-
-        console.debug('===============================');
-        const blinkTween = blinkTween(this.game, title);
-        console.debug('+++++++++++++++++++++++++++++++');
-        console.debug(blinkTween);
+        title.anchor.y = .5;       
+        const blinkingTitle = blinkTween(this.game, title);
 
         //solve blurry pixels
         this.game.renderer.renderSession.roundPixels = true;
