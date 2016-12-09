56fc16343d241b8141934276f5caea9ddfab13cd
Abraham Alcaina <abraham@weareadaptive.com>
2016-12-09 14:50:07 +0100

    eslint configured and applied

--- server/src/commands/joinGame.js
+++ server/src/commands/joinGame.js
@@ -1,31 +1,34 @@
-const { startRound } = require ('./round');
+const { startRound } = require('./round');
 
-const joinGame = (io, socket, games) => socket.on('joinGame', async function(data) {
-    const game = await await games.get(data.gameId);
-    console.log('join');
-    if (!game || game.guest) {
-        console.log('Game missing or already with a guest');
-        // TODO: if it's the same guest rejoin the play
-        io.to(socket.id).emit('gameJoined', {
-            gameId: 'gameId',
-            status: false
-        });
-        return;
-    }
-    game.guest = {
-        id: data.fbid,
-        socketId: socket.id,
-        wins: 0
-    };
-    const gameJoinedInfo = {
-        gameId: data.gameId,
-        status: true,
-        guestId: data.fbid,
-        ownerId: game.owner.id
-    };
-    games.set(data.gameId, game);
-    io.to(game.guest.socketId).emit('gameJoined', gameJoinedInfo);
-    io.to(game.owner.socketId).emit('gameJoined', gameJoinedInfo);
-    startRound(io, games, data.gameId);
+const joinGame = (io, socket, games) => socket.on('joinGame', async (data) => {
+  const game = await await games.get(data.gameId);
+  console.log('join');
+  if (!game || game.guest) {
+    console.log('Game missing or already with a guest');
+    // TODO: if it's the same guest rejoin the play
+    io.to(socket.id)
+      .emit('gameJoined', {
+        gameId: 'gameId',
+        status: false,
+      });
+    return;
+  }
+  game.guest = {
+    id: data.fbid,
+    socketId: socket.id,
+    wins: 0,
+  };
+  const gameJoinedInfo = {
+    gameId: data.gameId,
+    status: true,
+    guestId: data.fbid,
+    ownerId: game.owner.id,
+  };
+  games.set(data.gameId, game);
+  io.to(game.guest.socketId)
+    .emit('gameJoined', gameJoinedInfo);
+  io.to(game.owner.socketId)
+    .emit('gameJoined', gameJoinedInfo);
+  startRound(io, games, data.gameId);
 });
 module.exports = joinGame;
--- server/src/commands/startGame.js
+++ server/src/commands/startGame.js
@@ -1,26 +1,28 @@
 const uuid = require('uuid');
 
-const startGame = (io, socket, games) => socket.on('startGame', function(data) {
-    if (!data) {
-        io.to(socket.id).emit('error', {
-            message: 'no user data on game starts'
-        });
-    }
-    const gameId = uuid.v1();
-    const game = {
-        id: gameId,
-        numberOfWins: data.rounds || 2, // 2 victories = the best of 3
-        owner: {
-            id: data.fbid,
-            socketId: socket.id,
-            wins: 0
-        },
-        rounds: {}
-    };
-    games.set(gameId, game);
-    io.to(socket.id).emit('gameStarted', {
-        gameId,
-        rounds: game.numberOfWins
+const startGame = (io, socket, games) => socket.on('startGame', (data) => {
+  if (!data) {
+    io.to(socket.id)
+      .emit('error', {
+        message: 'no user data on game starts',
+      });
+  }
+  const gameId = uuid.v1();
+  const game = {
+    id: gameId,
+    numberOfWins: data.rounds || 2, // 2 victories = the best of 3
+    owner: {
+      id: data.fbid,
+      socketId: socket.id,
+      wins: 0,
+    },
+    rounds: {},
+  };
+  games.set(gameId, game);
+  io.to(socket.id)
+    .emit('gameStarted', {
+      gameId,
+      rounds: game.numberOfWins,
     });
 });
 module.exports = startGame;
