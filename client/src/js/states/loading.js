d09fb130e983205ab64aa7d0861c94a1247c992c
Bernat <bernatmv@gmail.com>
2016-12-09 10:57:52 +0100

    up-seized to original resolution of 750x1334

--- client/src/js/states/loading.js
+++ client/src/js/states/loading.js
@@ -11,21 +11,21 @@ class Loading extends Phaser.State {
         this.game.renderer.renderSession.roundPixels = true;
         // font style
         const style = {
-            font: "24px Cocon-Bold",
+            font: "42px Cocon-Bold",
             fill: "#fff",
             stroke: "#000",
-            strokeThickness: 6
+            strokeThickness: 8
         };
         // background
         addGradient(this.game, Properties.screen.backgroundGradient);
         // logo
-        const companyLogo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY - 200, "companyLogo", this);
+        const companyLogo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY - 400, "companyLogo", this);
         companyLogo.anchor.x = .5;
         companyLogo.anchor.y = .5;
         // text
-        const loadingText = this.game.add.text(this.game.width / 2, (this.game.height / 2) - 50, `Loading, please be patient...`, style);
+        const loadingText = this.game.add.text(this.game.world.centerX, this.game.world.centerY, `Loading, please be patient...`, style);
         loadingText.anchor.setTo(0.5, 0.5);
-        const preloadBar = this.game.add.text(this.game.width / 2, (this.game.height / 2) + 50, '0%', style);
+        const preloadBar = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 100, '0%', style);
         preloadBar.anchor.setTo(0.5, 0.5);
         // load images
         //this.game.load.image("hand-rock", require("../../assets/hands/rock.png"));
@@ -37,9 +37,9 @@ class Loading extends Phaser.State {
         this.game.load.image("icon-unknown", require("../../assets/icons/icon-unknown.png"));
         this.game.load.image("connection", require("../../assets/icons/connection_status.png"));
         //this.game.load.atlas("buttons", require("../../assets/buttons/circle/sprites.png"), null, buttonsJSON, Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
-        this.game.load.image("button-rock", require("../../assets/buttons/circle/button-rock.png"));
-        this.game.load.image("button-paper", require("../../assets/buttons/circle/button-paper.png"));
-        this.game.load.image("button-scissors", require("../../assets/buttons/circle/button-scissors.png"));
+        //this.game.load.image("button-rock", require("../../assets/buttons/circle/button-rock.png"));
+        //this.game.load.image("button-paper", require("../../assets/buttons/circle/button-paper.png"));
+        //this.game.load.image("button-scissors", require("../../assets/buttons/circle/button-scissors.png"));
         const loadCheck = () => {
             preloadBar.text = this.game.load.progress + '%';
         };
